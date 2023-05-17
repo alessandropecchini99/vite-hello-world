1cebef8a4b0ae4fd8d30e2962416565d92560e90
alessandropecchini99 <pecchini99@gmail.com>
2023-05-17 18:55:03 +0200

    First Vite app finish

---
+++ src/components/AppHero.vue
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+
+<template>
+  <h1>Seconda App Vite</h1>
+  <p>Ci si prova</p>
+</template>
+
+
+<style scoped>
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+
+<template>
+  <h1>La prima app Vite firmata Pek!</h1>
+  <p>Un po' complesso all'inizio, ma molto figo!</p>
+</template>
+
+
+<style scoped>
+
+</style>
