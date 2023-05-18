b500a4d96b02b28ddfadb11c3e414437f3702a9d
alessandropecchini99 <pecchini99@gmail.com>
2023-05-18 09:38:56 +0200

    better style (?)

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -4,11 +4,16 @@
 
 
 <template>
-  <h1>Seconda App Vite</h1>
+  <h1>Secondo Component Vite</h1>
   <p>Ci si prova</p>
 </template>
 
 
 <style scoped>
-
+    h1 {
+        color: rgb(255, 77, 77);
+    }
+    p {
+        color: rgb(255, 191, 0);
+    }
 </style>
--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -3,12 +3,36 @@
 </script>
 
 
+
 <template>
+
+  <div class="backgroundImg"></div>
+  
   <h1>La prima app Vite firmata Pek!</h1>
-  <p>Un po' complesso all'inizio, ma molto figo!</p>
+  <p>Figo!</p>
+
 </template>
 
 
+
 <style scoped>
+
+  h1 {
+    color: rgb(6, 112, 0);
+  }
+  p {
+    color: rgb(42, 92, 0);
+  }
+  .backgroundImg {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  background-image: url(doodle.webp);
+  opacity: 0.03;
+  filter: invert();
+  z-index: -1;
+}
 
 </style>
